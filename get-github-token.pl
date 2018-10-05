f79e1aaffea898dcdd756e5a76316e39bec2f5b4
Todd Rinaldo <toddr@cpanel.net>
2018-10-05 12:00:29 -0500

    Better diags when rt-to-github.pl fails

--- get-github-token.pl
+++ get-github-token.pl
@@ -6,12 +6,21 @@ use warnings;
 use Net::GitHub::V3;
 use IO::Prompt::Tiny qw/prompt/;
 
+print "Be sure to delete your existing token if you've not written it down\n";
+print "You can remove it here https://github.com/settings/tokens\n\n";
+
 my $user = prompt( "Github username:" );
 my $pass = prompt( "Github password:" );
 my $gh = Net::GitHub::V3->new( login => $user, pass => $pass );
 my $oauth = $gh->oauth;
-my $o = $oauth->create_authorization( {
+use Data::Dumper;
+
+my $o = eval { $oauth->create_authorization( {
     scopes => ['user', 'public_repo', 'repo', 'gist'],
     note   => 'Net::GitHub',
-} );
+} ) };
+
+#print "ERROR: $@\n";
+#print Dumper $oauth;
+#print Dumper $o;
 say $o->{token};
